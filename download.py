d52b31432a0cdb6ec48816c8f8b548449d0683a0
DENGBOYI <81019858+Aatrox103@users.noreply.github.com>
2026-04-09 22:44:49 +0800

    update

--- download.py
+++ download.py
@@ -5,11 +5,11 @@ from huggingface_hub import login
 # Set your own access token if needed
 # access_token_read = 'YOUR_OWN_ACCESS_TOKEN'
 # login(token = access_token_read)
-download_path = "./Llama3_1-8B-Base-LXR-8x"
+download_path = "./SAE/Llama3_1-8B-Base-LXR-8x"
 snapshot_download(repo_id="fnlp/Llama3_1-8B-Base-LXR-8x", local_dir=download_path)
 
-download_path = "./gemma-scope-2b-pt-res"
+download_path = "./SAE/gemma-scope-2b-pt-res"
 snapshot_download(repo_id="google/gemma-scope-2b-pt-res", local_dir=download_path)
 
-download_path = "./gemma-scope-9b-pt-res"
+download_path = "./SAE/gemma-scope-9b-pt-res"
 snapshot_download(repo_id="google/gemma-scope-9b-pt-res", local_dir=download_path)
